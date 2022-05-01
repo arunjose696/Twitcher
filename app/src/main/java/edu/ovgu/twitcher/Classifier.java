cf76aab108048df7686b467e1f978cc3481a88c1
arunj <arunjose696@gmail.com>
2022-05-01 10:48:00 +0200

    fixed issue with prediction, now predicting right label

--- app/src/main/java/edu/ovgu/twitcher/Classifier.java
+++ app/src/main/java/edu/ovgu/twitcher/Classifier.java
@@ -13,13 +13,20 @@ import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.label.Category;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import edu.ovgu.twitcher.ml.LiteModelAiyVisionClassifierBirdsV13;
 
 public class Classifier {
+    public int compare(Category c1, Category c2) {
+        // compare two instance of `Score` and return `int` as result.
+        return (int)(c1.getScore()-c2.getScore());
+    }
 
     public static void classify(ImageView imageView, Context context, TextInputEditText inputName){
+
+
         try {
 
             LiteModelAiyVisionClassifierBirdsV13 model = LiteModelAiyVisionClassifierBirdsV13.newInstance(context);
@@ -32,8 +39,17 @@ public class Classifier {
             // Runs model inference and gets result.
             LiteModelAiyVisionClassifierBirdsV13.Outputs outputs = model.process(image);
             List<Category> probability = outputs.getProbabilityAsCategoryList();
+
+
+
+            Collections.sort(probability, (o1, o2) -> Float.compare(o2.getScore() ,o1.getScore()));
+
+
             inputName.setText( probability.get(0).getLabel());
-            Log.i("0000000000000000000000" , probability.get(1).getLabel());
+
+
+
+
             // Releases model resources if no longer used.
             model.close();
         } catch (IOException e) {
